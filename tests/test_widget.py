297f7e6be2289e0578bf4e11d5a26fbfc4386e68
mikhail <beliyymishkan@gmail.com>
2024-08-18 20:57:21 +0300

    промежуточный этап доработки2

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,1 +1,21 @@
+import pytest
 
+from src.widget import get_date, mask_account_card
+
+
+@pytest.fixture
+def card_or_account_info():
+    return "Maestro 2345234545674567"
+
+
+@pytest.fixture
+def date_info():
+    return "2018-07-11T02:26:18.671407"
+
+
+def test_mask_account_card(card_or_account_info):
+    assert mask_account_card(card_or_account_info) == "Maestro 2345 23** **** 4567"
+
+
+def test_get_date(date_info):
+    assert get_date(date_info) == "11.07.2018"
